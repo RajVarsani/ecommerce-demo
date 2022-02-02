6f745ff2abe157680319dd411a66f0b4b1d9ea02
Raj <varasaniraj5@gmail.com>
2022-02-02 21:41:17 +0530

    updated routes for fields

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -1,5 +1,7 @@
-import customersRouter from './api/controllers/customers/router';
+import customersRouter from "./api/controllers/customers/router";
+import fieldRouter from "./api/controllers/fields/router";
 
 export default function routes(app) {
-  app.use('/api/v1/customer', customersRouter);
+  app.use("/api/v1/customer", customersRouter);
+  app.use("/api/v1/field", fieldRouter);
 }
