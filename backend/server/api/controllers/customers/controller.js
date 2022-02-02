fcb8405fcdc39f1d39b9acec4938a054b5d77a52
Raj <varasaniraj5@gmail.com>
2022-02-02 20:33:41 +0530

    Updated response checks in customer route controller

--- backend/server/api/controllers/customers/controller.js
+++ backend/server/api/controllers/customers/controller.js
@@ -5,35 +5,36 @@ export class Controller {
   async create(req, res, next) {
     try {
       const customer = await CustomerService.create(req.body);
-      res.json(customer);
+      return res.json(customer);
     } catch (err) {
-      if (err.name === "ValidationError") {
-        res.status(422).send(err);
-      } else if (err.name === "MongoError" && err.code === 11000) {
-        res.status(422).send(err.message);
-      } else {
-        next(err);
-      }
       l.error(err.toString());
+
+      if (err.name === "ValidationError") {
+        return res.status(422).send(err);
+      }
+      if (err.name === "MongoError" && err.code === 11000) {
+        return res.status(422).send(err.message);
+      }
+      return next(err);
     }
   }
 
-  async all(req, res) {
+  async all(req, res, next) {
     try {
       const customers = await CustomerService.all(req.query.type);
-      res.json(customers);
+      return res.json(customers);
     } catch (err) {
-      next(err);
+      return next(err);
     }
   }
 
   async byId(req, res) {
     try {
       const customer = await CustomerService.byId(req.params.id);
-      res.json(customer);
+      return res.json(customer);
     } catch (err) {
       l.error(err.toString());
-      res.status(404).send(err);
+      return res.status(404).send(err);
     }
   }
 }
