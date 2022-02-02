d5c65761cdf21d1c6344fd64245ac2f38350e57b
Raj <varasaniraj5@gmail.com>
2022-02-02 19:29:04 +0530

    Updated api.yml for swagger

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -1,5 +1,5 @@
-import examplesRouter from './api/controllers/examples/router';
+import customersRouter from './api/controllers/customers/router';
 
 export default function routes(app) {
-  app.use('/api/v1/examples', examplesRouter);
+  app.use('/api/v1/customer', customersRouter);
 }
