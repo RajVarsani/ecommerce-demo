2b764f3dc86d7be3f3f14d0a50fe63090ea33909
Raj <varasaniraj5@gmail.com>
2022-02-03 09:55:42 +0530

    Updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./Containers/Home";
 
 const App = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      {["/", "customer", "addcustomer", "addfield"].map((path, index) => (
+        <Route key={index} path={path} element={<Home />} />
+      ))}
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
