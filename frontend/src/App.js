997d36c5c49b878109c316cb9abe10249d5f0013
Raj <varasaniraj5@gmail.com>
2022-02-03 21:39:53 +0530

    Set up toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
-
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import styles from "./App.module.css";
+import "react-toastify/dist/ReactToastify.css";
+
 import Home from "./Containers/Home";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
-    <Routes>
-      {["/", "customer", "addcustomer", "addfield"].map((path, index) => (
-        <Route key={index} path={path} element={<Home />} />
-      ))}
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <>
+      <ToastContainer bodyClassName={styles.ToastBody} />
+      <Routes>
+        {["/", "customer/:id", "addcustomer", "addfield"].map((path, index) => (
+          <Route key={index} path={path} element={<Home />} />
+        ))}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 };
 
