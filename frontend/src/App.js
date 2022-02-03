450e61b0c2238fca0800025ffaecd0babe7c88fe
Raj <varasaniraj5@gmail.com>
2022-02-03 09:52:24 +0530

    Updated react-router-dom version - v6.2.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./Containers/Home";
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-    </Switch>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
   );
 };
 
