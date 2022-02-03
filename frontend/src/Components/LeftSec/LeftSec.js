f097ae0685dd7aaf4138f50d9dc0b81b9b29b6ec
Raj <varasaniraj5@gmail.com>
2022-02-03 18:43:29 +0530

    Updated left sec

--- frontend/src/Components/LeftSec/LeftSec.js
+++ frontend/src/Components/LeftSec/LeftSec.js
@@ -1,34 +1,42 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 import styles from "./LeftSec.module.css";
 
 import { leftSecData } from "./../../Utils/Constants/StaticData";
 
-import { Button } from "@mui/material";
 function LeftSec() {
   const navigate = useNavigate();
 
   return (
     <div className={styles.Wrapper}>
-      <Button
-        variant="contained"
-        className={styles.Button}
-        onClick={() => {
-          navigate("/addcustomer");
-        }}
-      >
-        {leftSecData.buttons.addCustomer}
-      </Button>
-      <Button
-        variant="outlined"
-        className={styles.Button}
-        onClick={() => {
-          navigate("/addfield");
-        }}
-      >
-        {leftSecData.buttons.addField}
-      </Button>
+      <Link to={"/"} className={styles.TopSec}>
+        <h1 className={styles.Title}>
+          <span>{leftSecData.title[0]}</span>
+          <span>{leftSecData.title[1]}</span>
+        </h1>
+      </Link>
+      <div className={styles.BottomSec}>
+        <Button
+          variant="contained"
+          className={styles.Button}
+          onClick={() => {
+            navigate("/addcustomer");
+          }}
+        >
+          {leftSecData.buttons.addCustomer}
+        </Button>
+        <Button
+          variant="outlined"
+          className={styles.Button}
+          onClick={() => {
+            navigate("/addfield");
+          }}
+        >
+          {leftSecData.buttons.addField}
+        </Button>
+      </div>
     </div>
   );
 }
