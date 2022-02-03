8378191fddb82fb90a65aead51ed01ceafe2ed3b
Raj <varasaniraj5@gmail.com>
2022-02-03 21:40:28 +0530

    Added rightsec static data

--- frontend/src/Utils/Constants/StaticData.js
+++ frontend/src/Utils/Constants/StaticData.js
@@ -1,6 +1,17 @@
 export const leftSecData = {
+  title: ["Shop", " Management"],
   buttons: {
     addCustomer: "Add Customer",
     addField: "Add Field",
+  },
+};
+
+export const rightSecData = {
+  tabs: ["electronics", "toys"],
+  keyLables: {
+    email: "email",
+    address: "address",
+    phone: "phone",
+    dob: "date of birth",
   },
 };
